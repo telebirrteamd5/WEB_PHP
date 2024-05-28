33303ec60f69865bf07624d8f95595a9299ccef7
eyu-dev <169823478+eyu-dev@users.noreply.github.com>
2024-05-28 23:09:48 +0300

    Initial commit
    Signed-off-by: eyu-dev <169823478+eyu-dev@users.noreply.github.com>

---
+++ config/env.php
@@ -0,0 +1,9 @@
+<?php 
+// Configuration Variables Set Here
+$ENV_Variables = array(
+    "baseUrl" => "https://196.188.120.3:38443/apiaccess/payment/gateway",
+    "fabricAppId" => "c4182ef8-9249-458a-985e-06d191f4d505",
+    "appSecret" => "fad0f06383c6297f545876694b974599",
+    "merchantAppId" => "930231098009602",
+    "merchantCode" =>  "101011"
+);
